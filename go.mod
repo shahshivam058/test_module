c631162c5566a80918c1b991124079e12d447a43
Shivam shah <59441391+shahshivam058@users.noreply.github.com>
2025-03-21 05:40:44 +0000

    Test module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shahshivam058/test_module
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package test_module 
+
+func test() {
+	return "Shivam Shah"
+}
+
+func testing_full() {
+	return "Hello world"
+}
